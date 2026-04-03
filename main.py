2a2ad8aafd81c0fbd9c020374ea3e39c1b41afef
giacomolap <giacomo.lapietra06@gmail.com>
2026-04-03 19:39:39 +0200

    Add recursive sum function for list elements

--- main.py
+++ main.py
@@ -12,4 +12,12 @@ def crea_lista(stringa):
 mia_lista = crea_lista("Scegli un numero da 0 a 9 o scrivi \"fine\" per terminare: ")
 print("La lista generata è: ", mia_lista)
 
-
+def somma_ricorsiva(listaDiNumeri):
+    if len(listaDiNumeri) == 0:
+        return 0
+    else:
+        primo_elemento = listaDiNumeri[0]
+        resto_della_lista = listaDiNumeri[1:]
+        return primo_elemento + somma_ricorsiva(resto_della_lista)
+print("La somma degli elementi della lista é: " + str(somma_ricorsiva(mia_lista)))  
+         
